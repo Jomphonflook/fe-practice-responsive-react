cb40c6b92589201ea490e171296f3674230751b9
Jomphonflook <jomphonisreal@gmail.com>
2023-06-16 11:37:03 +0700

    update

---
+++ fe-practice-responsive-react/src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+export const MainPage = () => {
+  return (
+   <div className='flex justify-center items-center mt-[100px]'>
+      <div className='w-[60px] h-[60px] bg-black sm:w-[200px] sm:h-[200px] sm:bg-red-700 md:bg-green-500'>
+          <h1 className='text-[20px] text-white sm:text-[64px] sm:text-black' >hello</h1>
+      </div>
+   </div>
+  )
+}
